eaa95c9dd75777b40c342ae3a3ff51c2a3ca11a7
xopherdeep <xopher@vi.co>
2023-08-21 15:32:47 -0700

    🚚 Support editing existing achievements

--- src/router/side-menu.routes.ts
+++ src/router/side-menu.routes.ts
@@ -10,7 +10,7 @@ const SideMenu = [
   {
     path: '/switch-profile',
     name: 'switch-profile',
-    component: () => import ('@/views/App/SideMenu/SwitchProfile/SwitchProfile.vue'),
+    component: () => import('@/views/App/SideMenu/SwitchProfile/SwitchProfile.vue'),
     meta: {
       requiresAuth: false
     }
@@ -18,77 +18,80 @@ const SideMenu = [
   {
     path: '/game-master',
     name: 'game-master',
-    component: () => import ('@/views/App/SideMenu/XpGameMaster/XpGameMaster.vue'),
+    component: () => import('@/views/App/SideMenu/XpGameMaster/XpGameMaster.vue'),
     children: [
       {
         path: '',
         name: 'xp-dashboard',
-        component: () => import ("../views/App/SideMenu/XpGameMaster/XpDashboard/XpDashboard.vue"),
+        component: () => import("../views/App/SideMenu/XpGameMaster/XpDashboard/XpDashboard.vue"),
       },
       {
         path: 'achievements',
         name: 'xp-achievements',
-        component: () => import ('@/views/App/SideMenu/XpGameMaster/XpAchievements/XpAchievements.vue'),
+        component: () => import('@/views/App/SideMenu/XpGameMaster/XpAchievements/XpAchievements.vue'),
       },
       {
         path: 'beastiary',
         name: 'xp-beastiary',
-        component: () => import ('@/views/App/SideMenu/XpGameMaster/XpBeastiary/XpBeastiary.vue'),
+        component: () => import('@/views/App/SideMenu/XpGameMaster/XpBeastiary/XpBeastiary.vue'),
       },
     ],
   },
   {
-    path: '/add-achievement',
-    component: () => import ('@/views/App/SideMenu/XpGameMaster/XpAchievements/XpAddAchievement/XpAddAchievement.vue'),
+    path: '/add-achievement/:id?',
+    name: 'xp-add-achievement',
+    component: () => import('@/views/App/SideMenu/XpGameMaster/XpAchievements/XpAddAchievement/XpAddAchievement.vue'),
+    props: true
+
   },
   {
     path: '/xp-membership',
-    component: () => import ('@/views/App/SideMenu/XpMembership/XpMembership.vue'),
+    component: () => import('@/views/App/SideMenu/XpMembership/XpMembership.vue'),
   },
   {
     path: '/xp-settings',
     name: 'xp-settings',
-    component: () => import ('@/views/App/SideMenu/XpSettings/XpSettings.vue'),
+    component: () => import('@/views/App/SideMenu/XpSettings/XpSettings.vue'),
     children: [
       {
         path: '',
         name: 'xp-settings-home',
-        component: () => import ('@/views/App/SideMenu/XpSettingsHome/XpSettingsHome.vue'),
+        component: () => import('@/views/App/SideMenu/XpSettingsHome/XpSettingsHome.vue'),
       },
       {
         path: 'general',
         name: 'xp-settings-general',
-        component: () => import ('@/views/App/SideMenu/XpSettings/components/GeneralSettings.vue'),
+        component: () => import('@/views/App/SideMenu/XpSettings/components/GeneralSettings.vue'),
       },
       {
         path: 'family',
         name: 'xp-settings-family',
-        component: () => import ('@/views/App/SideMenu/XpSettings/components/FamilySettings.vue'),
+        component: () => import('@/views/App/SideMenu/XpSettings/components/FamilySettings.vue'),
       },
       {
         path: 'sound',
         name: 'xp-settings-sound',
-        component: () => import ('@/views/App/SideMenu/XpSettings/components/SoundSettings.vue'),
+        component: () => import('@/views/App/SideMenu/XpSettings/components/SoundSettings.vue'),
       },
     ],
   },
   {
     path: '/xp-support',
-    component: () => import ('@/views/App/SideMenu/XpSupport/XpSupport.vue'),
+    component: () => import('@/views/App/SideMenu/XpSupport/XpSupport.vue'),
   },
   {
     path: '/about-xp',
-    component: () => import ('@/views/App/SideMenu/XpAbout/XpAbout.vue'),
+    component: () => import('@/views/App/SideMenu/XpAbout/XpAbout.vue'),
   },
   {
     path: '/log-out',
     name: 'log-out',
-    component: () => import ('@/views/App/SideMenu/LogOut/LogOut.vue'),
+    component: () => import('@/views/App/SideMenu/LogOut/LogOut.vue'),
   },
   {
     name: 'log-in',
     path: '/log-in',
-    component: () => import ('@/views/App/SideMenu/LogIn/LogIn.vue'),
+    component: () => import('@/views/App/SideMenu/LogIn/LogIn.vue'),
     meta: {
       requiresAuth: false
     }
