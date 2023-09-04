5226c59b981d59290a5a6dd15fccf138baaa4e0a
xopherdeep <xopher@vi.co>
2023-09-04 03:05:49 -0700

    aider: Replaced input fields for XP, GP, and AP with ion-segment components.
    # Aider chat conversation:
    
    ASSISTANT: To implement this feature, we need to modify the following file:
    
    - src/views/App/SideMenu/XpGameMaster/XpDoThisNotThat/components/XpAddDoDont.vue
    
    We will replace the current input fields for XP, GP, and AP with ion-segment components. Each segment will have predefined values and a custom option that will allow the user to input a custom value.
    
    Here are the changes:
    
    ```vue
    src/views/App/SideMenu/XpGameMaster/XpDoThisNotThat/components/XpAddDoDont.vue
    <<<<<<< HEAD
          <ion-item>
            <ion-label>XP</ion-label>
            <ion-input v-model.number="form.points.xp" type="number"></ion-input>
          </ion-item>
          <ion-item>
            <ion-label>GP</ion-label>
            <ion-input v-model.number="form.points.gp" type="number"></ion-input>
          </ion-item>
          <ion-item>
            <ion-label>AP</ion-label>
            <ion-input v-model.number="form.points.ap" type="number"></ion-input>
          </ion-item>
    =======
          <ion-item>
            <ion-label>XP</ion-label>
            <ion-segment v-model="form.points.xp">
              <ion-segment-button value="100">100</ion-segment-button>
              <ion-segment-button value="250">250</ion-segment-button>
              <ion-segment-button value="500">500</ion-segment-button>
              <ion-segment-button value="1000">1000</ion-segment-button>
            </ion-segment>
          </ion-item>
          <ion-item>
            <ion-label>GP</ion-label>
            <ion-segment v-model="form.points.gp">
              <ion-segment-button value="10">10</ion-segment-button>
              <ion-segment-button value="25">25</ion-segment-button>
              <ion-segment-button value="50">50</ion-segment-button>
              <ion-segment-button value="100">100</ion-segment-button>
            </ion-segment>
          </ion-item>
          <ion-item>
            <ion-label>AP</ion-label>
            <ion-segment v-model="form.points.ap">
              <ion-segment-button value="1">1</ion-segment-button>
              <ion-segment-button value="3">3</ion-segment-button>
              <ion-segment-button value="5">5</ion-segment-button>
              <ion-segment-button value="10">10</ion-segment-button>
            </ion-segment>
          </ion-item>
    >>>>>>> updated
    ```
    
    Please note that the v-model directive is used to create two-way data bindings on form input, textarea, and select elements. It automatically picks the correct way to update the element based on the input type. In this case, it will update the form.points.xp, form.points.gp, and form.points.ap values based on the selected segment button.

--- src/views/App/SideMenu/XpGameMaster/XpDoThisNotThat/components/XpAddDoDont.vue
+++ src/views/App/SideMenu/XpGameMaster/XpDoThisNotThat/components/XpAddDoDont.vue
@@ -12,15 +12,30 @@
       </ion-segment>
       <ion-item>
         <ion-label>XP</ion-label>
-        <ion-input v-model.number="form.points.xp" type="number"></ion-input>
+        <ion-segment v-model="form.points.xp">
+          <ion-segment-button value="100">100</ion-segment-button>
+          <ion-segment-button value="250">250</ion-segment-button>
+          <ion-segment-button value="500">500</ion-segment-button>
+          <ion-segment-button value="1000">1000</ion-segment-button>
+        </ion-segment>
       </ion-item>
       <ion-item>
         <ion-label>GP</ion-label>
-        <ion-input v-model.number="form.points.gp" type="number"></ion-input>
+        <ion-segment v-model="form.points.gp">
+          <ion-segment-button value="10">10</ion-segment-button>
+          <ion-segment-button value="25">25</ion-segment-button>
+          <ion-segment-button value="50">50</ion-segment-button>
+          <ion-segment-button value="100">100</ion-segment-button>
+        </ion-segment>
       </ion-item>
       <ion-item>
         <ion-label>AP</ion-label>
-        <ion-input v-model.number="form.points.ap" type="number"></ion-input>
+        <ion-segment v-model="form.points.ap">
+          <ion-segment-button value="1">1</ion-segment-button>
+          <ion-segment-button value="3">3</ion-segment-button>
+          <ion-segment-button value="5">5</ion-segment-button>
+          <ion-segment-button value="10">10</ion-segment-button>
+        </ion-segment>
       </ion-item>
       <ion-item>
         <ion-label>Notes</ion-label>
