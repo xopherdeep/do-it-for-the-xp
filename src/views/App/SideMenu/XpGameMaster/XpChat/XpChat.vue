480bbfae8863dbd7f208a7914e817bb071459d0a
xopherdeep <xopher@vi.co>
2025-04-12 12:37:32 -0700

    feat: Add XpChat component with messaging functionality and UI styling

---
+++ src/views/App/SideMenu/XpGameMaster/XpChat/XpChat.vue
@@ -0,0 +1,46 @@
+<template>
+  <ion-page :class="$options.name">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Family Chat</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <div class="message-container">
+        <!-- Messages will be displayed here -->
+        <div v-for="(message, index) in messages" :key="index" 
+          :class="['message-bubble', message.isOwnMessage ? 'own-message' : 'other-message']">
+          <div class="message-avatar">
+            <ion-avatar>
+              <img :src="message.avatar || 'https://ionicframework.com/docs/img/demos/avatar.svg'" />
+            </ion-avatar>
+          </div>
+          <div class="message-content">
+            <div class="message-header">
+              <strong>{{ message.sender }}</strong>
+              <small>{{ formatTime(message.timestamp) }}</small>
+            </div>
+            <div class="message-text">{{ message.text }}</div>
+          </div>
+        </div>
+      </div>
+    </ion-content>
+    <ion-footer>
+      <ion-toolbar>
+        <ion-item>
+          <ion-input 
+            v-model="newMessage" 
+            placeholder="Type a message..."
+            @keyup.enter="sendMessage"
+          ></ion-input>
+          <ion-button slot="end" @click="sendMessage" :disabled="!newMessage.trim()">
+            <ion-icon slot="icon-only" name="send"></ion-icon>
+          </ion-button>
+        </ion-item>
+      </ion-toolbar>
+    </ion-footer>
+  </ion-page>
+</template>
+
+<script src="./XpChat" />
+<style lang="scss" src="./_XpChat.scss" scoped />
