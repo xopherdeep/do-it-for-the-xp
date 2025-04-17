93cb23c6adb8d0614e0e4f42cd89b738cc5e1f63
xopherdeep <xopher@vi.co>
2025-04-17 11:34:12 -0700

    feat: Add shooting stars animation to WorldMoon component; enhance night sky visuals with dynamic effects

--- src/views/Console/MyPortal/WorldMap/WorldMoon/WorldMoon.vue
+++ src/views/Console/MyPortal/WorldMap/WorldMoon/WorldMoon.vue
@@ -13,6 +13,13 @@
     </ion-header> -->
 
     <ion-content :fullscreen="true">
+      <div class="night-sky">
+        <div class="shooting-star shooting-star1"></div>
+        <div class="shooting-star shooting-star2"></div>
+        <div class="shooting-star shooting-star3"></div>
+        <div class="shooting-star shooting-star4"></div>
+        <div class="shooting-star shooting-star5"></div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
