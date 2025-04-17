5563efcfcf881bd45f802b53c4552f5732a8313a
xopherdeep <xopher@vi.co>
2025-04-17 11:38:17 -0700

    feat: Add storm effects to WorldSwamps component; include rain layers, lightning flashes, rain puddles, and mist animations

--- src/views/Console/MyPortal/WorldMap/WorldSwamps/WorldSwamps.vue
+++ src/views/Console/MyPortal/WorldMap/WorldSwamps/WorldSwamps.vue
@@ -13,6 +13,38 @@
     </ion-header> -->
 
     <ion-content :fullscreen="true">
+      <!-- Storm effects container -->
+      <div class="storm-effects">
+        <!-- Rain layers for sideways rain effect -->
+        <div class="rain-container">
+          <div class="rain-layer rain-layer-1">
+            <div v-for="i in 50" :key="`rain-1-${i}`" class="rain-drop"></div>
+          </div>
+          <div class="rain-layer rain-layer-2">
+            <div v-for="i in 50" :key="`rain-2-${i}`" class="rain-drop"></div>
+          </div>
+          <div class="rain-layer rain-layer-3">
+            <div v-for="i in 50" :key="`rain-3-${i}`" class="rain-drop"></div>
+          </div>
+        </div>
+        
+        <!-- Lightning effects -->
+        <div class="lightning-container">
+          <div class="lightning-flash lightning-1"></div>
+          <div class="lightning-flash lightning-2"></div>
+          <div class="lightning-flash lightning-3"></div>
+        </div>
+        
+        <!-- Rain puddles on the ground -->
+        <div class="rain-puddles">
+          <div v-for="i in 20" :key="`puddle-${i}`" class="puddle"></div>
+        </div>
+        
+        <!-- Swamp mist effects -->
+        <div class="mist-container">
+          <div v-for="i in 8" :key="`mist-${i}`" class="mist"></div>
+        </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
