7c30d9c216835958b9262a98fded47a0760dbaed
xopherdeep <xopher@vi.co>
2025-04-29 12:23:38 -0700

    feat: Enhance settings page with dynamic items and improved styling

--- src/views/App/SideMenu/XpSettings/components/XpSettingsHome/XpSettingsHome.ts
+++ src/views/App/SideMenu/XpSettings/components/XpSettingsHome/XpSettingsHome.ts
@@ -8,17 +8,85 @@ import {
   pizzaSharp
 } from "ionicons/icons"
 
+interface SettingsItem {
+  id: string;
+  title: string;
+  description: string;
+  icon: string;
+  route: string;
+}
+
 export default defineComponent({
   name: 'xp-settings-home',
   mixins: [ionic],
   setup() {
     // code
     
+    const settingsItems: SettingsItem[] = [
+      {
+        id: 'general',
+        title: 'General',
+        description: 'Customize your family setup and general preferences from here.',
+        icon: require('@/assets/icons/settings.png'),
+        route: '/xp-settings/general'
+      },
+      {
+        id: 'family',
+        title: 'Family Settings',
+        description: 'Add kids, invite adults and manage your family settings from here.',
+        icon: require('@/assets/icons/team.png'),
+        route: '/xp-settings/family'
+      },
+      {
+        id: 'chore',
+        title: 'Chore',
+        description: 'From photo proof to late penalty, everything to personalize your chore system',
+        icon: require('@/assets/icons/innovation.png'),
+        route: '/xp-settings/chore'
+      },
+      {
+        id: 'notifications',
+        title: 'Notifications',
+        description: 'Setup who gets notified and when they get notified here',
+        icon: require('@/assets/icons/notification.png'),
+        route: '/xp-settings/notifications'
+      },
+      {
+        id: 'reward',
+        title: 'Reward',
+        description: 'Your Gold Point to current conversion, cast-out and reward approval can be found here',
+        icon: require('@/assets/icons/new.png'),
+        route: '/xp-settings/reward'
+      },
+      {
+        id: 'party',
+        title: 'Party',
+        description: 'All settings pertaining to your party can be found here...',
+        icon: require('@/assets/icons/collaboration.png'),
+        route: '/xp-settings/party'
+      },
+      {
+        id: 'sound',
+        title: 'Sound',
+        description: 'Adjust your sound settings here.',
+        icon: require('@/assets/icons/listener.png'),
+        route: '/xp-settings/sound'
+      },
+      {
+        id: 'theme',
+        title: 'Theme',
+        description: 'Customize app appearance, UI style, and game theme',
+        icon: require('@/assets/icons/settings.png'),
+        route: '/xp-settings/theme'
+      }
+    ];
+    
     return {
       arrowBack,
       requireIconImg,
       pizzaOutline,
       pizzaSharp,
+      settingsItems
     };
   },
 })
--- src/views/App/SideMenu/XpSettings/components/XpSettingsHome/XpSettingsHome.vue
+++ src/views/App/SideMenu/XpSettings/components/XpSettingsHome/XpSettingsHome.vue
@@ -19,125 +19,35 @@
       :fullscreen="true"
       class="rpg-box bg-slide ion-padding"
     >
-      <ion-card class="md:max-w-[80%] max-w-[95%]">
-        <ion-list>
-          <ion-item
-            button
-            :routerLink="`/xp-settings/general`"
-            detail="true"
+      <ion-grid class="max-w-5xl">
+        <ion-row>
+          <ion-col
+            size="12"
+            size-md="4"
+            v-for="item in settingsItems"
+            :key="item.id"
           >
-            <ion-thumbnail class="float-right">
-              <ion-img :src="requireIconImg('./settings.png')" />
-            </ion-thumbnail>
-            <ion-label>
-              <h1>General</h1>
-              <p>
-                Customize your family setup and general preferences from here.
-              </p>
-            </ion-label>
-          </ion-item>
-          <ion-item
-            button
-            :routerLink="`/xp-settings/family`"
-            detail="true"
-            class="flex flex-row gap-4 justify-between"
-          >
-            <ion-thumbnail slot="start">
-              <ion-img :src="requireIconImg('./team.png')" />
-            </ion-thumbnail>
-            <ion-label>
-              <h1>Family Settings</h1>
-              <p>
-                Add kids, invite adults and manage your family settings from here.
-              </p>
-            </ion-label>
-          </ion-item>
-          <ion-item
-            button
-            :routerLink="`/xp-settings/chore`"
-            detail="true"
-          >
-            <ion-thumbnail slot="start">
-              <ion-img :src="requireIconImg('./innovation.png')" />
-            </ion-thumbnail>
-            <ion-label>
-              <h1>Chore</h1>
-              <p>
-                From photo proof to late penalty, everything to personalize your
-                chore system
-              </p>
-            </ion-label>
-          </ion-item>
-          <ion-item
-            button
-            :routerLink="`/xp-settings/notifications`"
-            detail="true"
-          >
-            <ion-thumbnail slot="start">
-              <ion-img :src="requireIconImg('./notification.png')" />
-            </ion-thumbnail>
-            <ion-label>
-              <h1>Notifications</h1>
-              <p>Setup who gets notified and when they get notified here</p>
-            </ion-label>
-          </ion-item>
-          <ion-item
-            button
-            :routerLink="`/xp-settings/reward`"
-            detail="true"
-          >
-            <ion-thumbnail slot="start">
-              <ion-img :src="requireIconImg('./new.png')" />
-            </ion-thumbnail>
-            <ion-label>
-              <h1>Reward</h1>
-              <p>
-                Your Gold Point to current conversion, cast-out and reward
-                approval can be found here
-              </p>
-            </ion-label>
-          </ion-item>
-          <ion-item
-            button
-            :routerLink="`/xp-settings/party`"
-            detail="true"
-          >
-            <ion-thumbnail slot="start">
-              <ion-img :src="requireIconImg('./collaboration.png')" />
-            </ion-thumbnail>
-            <ion-label>
-              <h1>Party</h1>
-              <p>All settings pertaining to your party can be found here...</p>
-            </ion-label>
-          </ion-item>
-          <ion-item
-            router-link="/xp-settings/sound"
-            button
-            detail="true"
-          >
-            <ion-thumbnail slot="start">
-              <ion-img :src="requireIconImg('./listener.png')" />
-            </ion-thumbnail>
-            <ion-label>
-              <h1>Sound</h1>
-              <p>Adjust your sound settings here.</p>
-            </ion-label>
-          </ion-item>
-          <ion-item
-            button
-            :routerLink="`/xp-settings/theme`"
-            detail="true"
-          >
-            <ion-thumbnail slot="start">
-              <ion-img :src="requireIconImg('./settings.png')" />
-            </ion-thumbnail>
-            <ion-label>
-              <h1>Theme</h1>
-              <p>Customize app appearance, UI style, and game theme</p>
-            </ion-label>
-          </ion-item>
-        </ion-list>
-      </ion-card>
+            <ion-card
+              button
+              :routerLink="item.route"
+              class="settings-card"
+            >
+              <ion-card-header class="ion-text-center">
+                <ion-thumbnail class="mx-auto">
+                  <ion-img :src="item.icon" />
+                </ion-thumbnail>
+                <ion-card-title>
+                  {{ item.title }}
+                </ion-card-title>
+              </ion-card-header>
+              <hr />
+              <ion-card-content>
+                {{ item.description }}
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
