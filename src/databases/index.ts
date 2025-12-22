63ecbe8c0adf89fc96054482ae05b36b5a3d3330
Xopher <xopher@vi.co>
2025-12-22 04:11:07 -0700

    feat: Introduce XpBestiary and XpTempleCreator features, refactor battle engine, and update store modules while removing old database files.

--- src/databases/index.ts
+++
@@ -1,4 +0,0 @@
-export { ProfileDb } from './ProfileDb';
-export { AchievementDb } from './AchievementDb'
-export { DbStorageApi } from './DbStorageApi'
-// export { StorageApi } from './storage.api'
